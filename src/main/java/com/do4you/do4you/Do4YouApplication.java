51abd8289bbe87eb0cb1e82272b177004ab8f567
hyeri <cos850@naver.com>
2024-02-03 22:32:33 +0900

    Initial commit

---
+++ src/main/java/com/do4you/do4you/Do4YouApplication.java
@@ -0,0 +1,13 @@
+package com.do4you.do4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Do4YouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Do4YouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/do4you/do4you/Do4YouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.do4you.do4you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Do4YouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
