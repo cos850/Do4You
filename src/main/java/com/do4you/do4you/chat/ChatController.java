c37e41aed644c5c134852d94b393f37ec7854385
hyeri <cos850@naver.com>
2024-03-19 21:58:12 +0900

    thymeleaf 에 맞게 코드 변경 (리액트로 이동 후 삭제하기)

--- src/main/java/com/do4you/do4you/chat/ChatController.java
+++ src/main/java/com/do4you/do4you/chat/ChatController.java
@@ -7,6 +7,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
@@ -15,6 +16,11 @@ public class ChatController {
     @Autowired
     public ChatController(MessageService messageService) {
         this.messageService = messageService;
+    }
+
+    @GetMapping("/chat")
+    public String getHome(){
+        return "chat";  // thymeleaf 템플릿을 사용하므로 추가(리액트로 옮긴 후 제거). {prefix}/chat.{suffix} 에 해당하는 view 를 찾아서 반환
     }
 
     @MessageMapping("/message/{roomId}")
