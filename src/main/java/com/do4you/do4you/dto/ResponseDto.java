ef8abfd740b373c3e36dec724bce65dd1791b339
Soiiii <cojomkr33@gmail.com>
2024-05-15 15:55:30 +0900

    응답 dto 추가

---
+++ src/main/java/com/do4you/do4you/dto/ResponseDto.java
@@ -0,0 +1,14 @@
+package com.do4you.do4you.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto<T> {
+    T data;
+    String id;
+
+}
